6891513bee453cb9fbe29e78c37c4fdf19ec333d
JukisYuri <23130092@st.hcmuaf.edu.vn>
2026-06-04 23:58:13 +0700

    Initial commit: Setup project

---
+++ prototype/src/test/kotlin/group4/prototype/PrototypeApplicationTests.kt
@@ -0,0 +1,13 @@
+package group4.prototype
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrototypeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
